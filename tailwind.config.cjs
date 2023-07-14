9675c6cb545a4a7e7104ec5049cb3e5eca02d168
ZN <askadar.ru@gmail.com>
2023-07-14 16:18:52 +0300

    most of scooty done added necessary assets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,25 +1,22 @@
-const colors = require("tailwindcss/colors");
+const colors = require('tailwindcss/colors')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: {
-			transparent: 'transparent',
-			current: 'currentColor',
-			gray: colors.gray,
-
-			sgray: {
-				100: '#17181A',
-				200: '#414449',
-				300: '#8B938D',
-				500: '#DBDBDB',
-				700: '#D8D8D8',
-				800: '#FAFAFA',
-				900: '#FFFFFF',
+		extend: {
+			colors: {
+				sgray: {
+					100: '#17181A',
+					200: '#414449',
+					300: '#8B938D',
+					500: '#DBDBDB',
+					700: '#D8D8D8',
+					800: '#FAFAFA',
+					900: '#FFFFFF',
+				},
 			},
 		},
-		extend: {},
 	},
 	plugins: [],
 }
